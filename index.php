2a25355ce347d139f5d2effcbd13821162ca1437
naryzhnaj <naryzhnaj@gmail.com>
2018-09-13 12:34:51 +0300

    задание 4 подправлено

--- index.php
+++ index.php
@@ -133,22 +133,22 @@ $show_complete_tasks = rand(0, 1);
                         <td class="task__controls">
                         </td>
                       </tr>
+
+                      <tr class="tasks__item task task--completed">
+                          <td class="task__select">
+                              <label class="checkbox task__checkbox">
+                                  <input class="checkbox__input visually-hidden task__checkbox" type="checkbox" checked>
+                                  <span class="checkbox__text">Сделать главную страницу Дела в порядке</span>
+                              </label>
+                          </td>
+
+                          <td class="task__file">
+                              <a class="download-link" href="#">Home.psd</a>
+                          </td>
+
+                          <td class="task__date"></td>
+                      </tr>
                     <?php endif; ?>
-
-                    <tr class="tasks__item task task--completed">
-                        <td class="task__select">
-                            <label class="checkbox task__checkbox">
-                                <input class="checkbox__input visually-hidden task__checkbox" type="checkbox" checked>
-                                <span class="checkbox__text">Сделать главную страницу Дела в порядке</span>
-                            </label>
-                        </td>
-
-                        <td class="task__file">
-                            <a class="download-link" href="#">Home.psd</a>
-                        </td>
-
-                        <td class="task__date"></td>
-                    </tr>
                 </table>
             </main>
         </div>
