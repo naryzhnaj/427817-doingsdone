931afe352bb81228a158fd174a9769f8d95e9ed1
naryzhnaj <naryzhnaj@gmail.com>
2018-09-21 14:56:16 +0300

    задание 9

--- templates/index.php
+++ templates/index.php
@@ -26,16 +26,18 @@
 <table class="tasks">
     <?php foreach ($tasks as $task): ?>
         <?php if ( $show_complete_tasks || !$task['done'] ): ?> 
-            <tr class="tasks__item task <?php if ( $task['done'] ):?> task--completed <?php endif; ?>">
+            <tr class="tasks__item task 
+                <?php if ( $task['date'] != 'Нет' && floor( (strtotime($task['date'] ) - time() ) / 3600) <= 24 ):?> task--important <?php endif; ?>
+                <?php if ( $task['done'] ):?> task--completed <?php endif; ?>">
                 <td class="task__select">
                 <label class="checkbox task__checkbox">
                     <input class="checkbox__input visually-hidden" type="checkbox"  <?php if ( $task['done'] ): ?>checked<?php endif; ?>>
-                    <span class="checkbox__text"><?=$task['name'];?></span>
+                    <span class="checkbox__text"><?= htmlspecialchars($task['name']); ?></span>
                 </label>
                 </td>
-                <td class="task__date"><?=$task['date'];?></td>
+                <td class="task__date"><?= htmlspecialchars($task['date']); ?></td>
                 <td class="task__controls">
-                    <?=$task['category'];?>
+                    <?= htmlspecialchars($task['category']); ?>
                 </td>
             </tr>  
         <?php endif; ?>
