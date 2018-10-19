681ff0fb32693c4ffabb9fc8aee883d5a042c209
naryzhnaj <naryzhnaj@gmail.com>
2018-10-19 15:52:03 +0300

    добавлен поиск

--- templates/index.php
+++ templates/index.php
@@ -1,7 +1,7 @@
 <h2 class="content__main-heading">Список задач</h2>
 
-<form class="search-form" action="index.php" method="post">
-    <input class="search-form__input" type="text" name="" value="" placeholder="Поиск по задачам">
+<form class="search-form" action="index.php" method="get">
+    <input class="search-form__input" type="text" name="task_search" value="" placeholder="Поиск по задачам">
 
     <input class="search-form__submit" type="submit" name="" value="Искать">
 </form>
@@ -45,3 +45,6 @@
         <?php endif; ?>
     <?php endforeach; ?>
 </table>
+<?php if (isset($errors['empty_tasks'])):?>
+    <p>Ничего не найдено по вашему запросу</p>
+<?php endif;?>
